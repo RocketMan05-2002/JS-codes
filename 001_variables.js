29f42247ead5481736082f2536782ae750de154a
RocketMan05-2002 <156697759+RocketMan05-2002@users.noreply.github.com>
2024-11-23 01:24:55 +0530

    Add files via upload

---
+++ 001_variables.js
@@ -0,0 +1,14 @@
+console.log("abhinay")
+const accId = 1334567
+let email = "abhinayshambharkar1@gmail.com"
+var pwd = "12345"
+acccity = "nagpur"
+
+//accId = 2;
+email = "asas@gmail.com"
+pwd = "21212121"
+acccity = "pune"
+console.table([accId , email , pwd , acccity])
+
+// js me const se constant varaible ko likha jata h
+// aur baaki ko let, var se likh sakte h, so whats the difference?
